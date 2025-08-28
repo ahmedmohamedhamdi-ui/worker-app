06563ea1c41c8fd0009b9cce74189eb5a0361ac3
ahmedmohamedhamdi-ui <ahmed.mohamed.hamdi@gmail.com>
2025-08-28 09:16:59 +0300

    update project

--- src/Components/Workers/Workers.jsx
+++ src/Components/Workers/Workers.jsx
@@ -225,7 +225,7 @@ export const Workers = () => {
   return (
     <div
       className="container-fluid mt-5 workers-page"
-      style={{ width: "85%", margin: "auto" }}
+      style={{ width: "95%", margin: "auto" }}
     >
       <h2 className="text-center text-white p-3 rounded shadow">
         قائمة العمال
